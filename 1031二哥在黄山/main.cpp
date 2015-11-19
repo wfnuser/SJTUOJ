fc61eb0d2d1ce8da1c66016ca9acc83073500905
wfnuser <wfnuser@126.com>
2015-11-20 00:35:04 +0800

    the 1031二哥爬黄山

--- 1031二哥在黄山/main.cpp
+++ 1031二哥在黄山/main.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-int main() {
+int main(){
 	int n;
+	int path;
 	int i,j;
 	int map[111][111]={0};
 	int min,max;
@@ -28,14 +29,17 @@ int main() {
 	}
 	int flag = 0;
 	int wave = 1000000;
+	int father[20000]={0};
 	for(s=min;s<=max;s++)
 	{
 		for(e=s;e<=max;e++){
+			//cout<<"---------------------------"<<endl;
 			//cout<<"start from "<<s<<" to "<<e<<endl;
 			for(i=0;i<=n-1;i++){
 				for(j=0;j<=n-1;j++){
 					visit[i][j] = 0;
 					queue[i*n+j] = 0;
+					father[i*n+j] = 0;
 					if(map[i][j]>=s&&map[i][j]<=e){state[i][j] = 1;}
 					else {state[i][j] = 0;}
 					//cout<<state[i][j]<<" ";
@@ -53,18 +57,47 @@ int main() {
 					visit[row][col+1] = 1;
 					if(state[row][col+1] == 1){
 						queue[head++] = n*(row) + col + 1;
+						father[queue[head-1]] = queue[tail];
 					}	
 				}		
 				if(row<n-1&&col<n&&!visit[row+1][col]){
 					visit[row+1][col] = 1;
 					if(state[row+1][col] == 1){
 						queue[head++] = n*(row+1) + col;
+						father[queue[head-1]] = queue[tail];
+					}	
+				}	
+				if(col>0&&!visit[row][col-1]){
+					visit[row][col-1] = 1;
+					if(state[row][col-1] == 1){
+						queue[head++] = n*(row) + col - 1;
+						father[queue[head-1]] = queue[tail];
+					}	
+				}		
+				if(row>0&&!visit[row-1][col]){
+					visit[row-1][col] = 1;
+					if(state[row-1][col] == 1){
+						queue[head++] = n*(row-1) + col;
+						father[queue[head-1]] = queue[tail];
 					}	
 				}	
 				tail++;	
 			}	
+			if(flag){
+				//cout<<"the path is:"<<endl;
+				path = queue[tail];
+				while(path!=0){
+					//cout<<"("<<(path/n)<<","<<(path-(path/n)*(n))<<")"<<" ";
+					path = father[path];
+				}
+				//cout<<endl;
+				//cout<<"wave is : "<<wave<<endl;
+			}
 			flag = 0;
+			//cout<<"-----------------------"<<endl;
+			//cout<<endl;
 		}
 	}
 	cout<<wave;
+	//cout<<endl;
 }
